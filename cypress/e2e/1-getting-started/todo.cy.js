11d7b1f9401c60c9ddb96fa849f3e273a0ce5928
Guille RocheE <guillermorochea@gmail.com>
2023-12-02 16:40:59 +0100

    asdasdqawdqwdqwdq

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -76,7 +76,7 @@ describe('example to-do app', () => {
     // Once we get that element, we can assert that it has the completed class.
     cy.contains('Pay electric bill')
       .parents('li')
-      .should('have.class', 'completed')
+      .should('not.have.class', 'completed')
   })
 
   context('with a checked task', () => {
